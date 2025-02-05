b40c37603910ad3af6c88afe29fe54a668ebf56f
Camila-RG <camilaramosgomes@gmail.com>
2025-02-04 21:36:45 -0300

    Componentes declarados e inicializados

--- Atividade02_OneShot.c
+++ Atividade02_OneShot.c
@@ -1,14 +1,30 @@
+// Bibliotecas necessárias para o projeto
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/time.h" 
 
+// Definição dos pinos dos leds e do botão
+#define LED_G_PIN 11
+#define LED_B_PIN 12
+#define LED_R_PIN 13
 
+#define BUTTON_PIN 5
 
-int main()
-{
-    stdio_init_all();
+// Função para inicializar os LEDs e botões
+void inicializacaocomponentes() {
+    gpio_init(LED_G_PIN);
+    gpio_set_dir(LED_G_PIN, GPIO_OUT);
+    gpio_put(LED_G_PIN, 0);
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+    gpio_init(LED_B_PIN);
+    gpio_set_dir(LED_B_PIN, GPIO_OUT);
+    gpio_put(LED_B_PIN, 0);
+
+    gpio_init(LED_R_PIN);
+    gpio_set_dir(LED_R_PIN, GPIO_OUT);
+    gpio_put(LED_R_PIN, 0);
+
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN);
 }
