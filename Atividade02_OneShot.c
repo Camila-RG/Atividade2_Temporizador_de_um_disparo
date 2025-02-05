1b4cd36223fa55997b478b1ad9b99918de711a0c
Camila-RG <camilaramosgomes@gmail.com>
2025-02-04 22:27:30 -0300

    Função de callback definida

--- Atividade02_OneShot.c
+++ Atividade02_OneShot.c
@@ -10,6 +10,8 @@
 
 #define BUTTON_PIN 5
 
+int contador = 0; // Contador
+
 // Função para inicializar os LEDs e botões
 void inicializacaocomponentes() {
     gpio_init(LED_G_PIN);
@@ -29,14 +31,39 @@ void inicializacaocomponentes() {
     gpio_pull_up(BUTTON_PIN); // Habilita o resistor pull-up interno para o pino do botão.
 }
 
-int main()
-{
-    stdio_init_all();
-    inicializacaocomponentes();
+int64_t turn_off_callback(alarm_id_t id, void *user_data) {
+         if (contador == 0) {
+        gpio_put(LED_G_PIN, 1);
+        gpio_put(LED_B_PIN, 1);
+        gpio_put(LED_R_PIN, 1);
 
-    while (true)
-    {
+        contador = 1;
+    } else if (contador == 1) { 
+        gpio_put(LED_G_PIN, 0);
+        gpio_put(LED_B_PIN, 1);
+        gpio_put(LED_R_PIN, 1);
 
+        contador = 2;
+    } else {
+        gpio_put(LED_G_PIN, 0);
+        gpio_put(LED_B_PIN, 0);
+        gpio_put(LED_R_PIN, 1);
+
+        contador = 0;
     }
     return 0;
+}
+
+int main() {
+    stdio_init_all();
+    inicializacaocomponentes(); // Inicializa os LEDs e o botão
+
+    // Rotina principal
+    while (true) {
+        // Agenda um alarme para desligar o LED após 3 segundos (3000 ms).
+        add_alarm_in_ms(3000, turn_off_callback, NULL, false);
+        // Introduz uma pequena pausa de 10 ms para reduzir o uso da CPU.
+        sleep_ms(10);
+    }
+    return 0;  // Retorna 0, embora o loop seja infinito
 }
